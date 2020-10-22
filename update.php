64ac1db792bfa804a7dc21476a958fe383471399
tomek-ch <54998098+tomek-ch@users.noreply.github.com>
2020-10-22 22:03:39 +0200

    Add the option to edit records

--- update.php
+++ update.php
@@ -2,6 +2,7 @@
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $mysqli = new mysqli('localhost', 'root', '', 'phonebook') or die(mysqli_error($mysqli));
 
+        $id = stripslashes(trim(htmlspecialchars($_POST['company-id'])));
         $name = stripslashes(trim(htmlspecialchars($_POST['company-name'])));
         $description = stripslashes(trim(htmlspecialchars($_POST['company-description'])));
         $phone = stripslashes(trim(htmlspecialchars($_POST['company-phone-number'])));
@@ -10,8 +11,14 @@
         $address = stripslashes(trim(htmlspecialchars($_POST['company-address'])));
 
         $mysqli->query(
-            "INSERT INTO companies VALUES
-            (null, '$name', '$description', '$phone', '$email', '$website', '$address')"
+            "UPDATE companies SET
+            name = '$name',
+            description = '$description',
+            phone = '$phone',
+            email = '$email',
+            website = '$website',
+            address = '$address'
+            WHERE id = {$id}"
             ) or die($mysqli->error);
     } else {
         http_response_code(404);
